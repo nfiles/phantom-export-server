08f3fcb4a8f96ad403abc8804ee91cfd03b07206
Nathan Files <nathanwfiles@gmail.com>
2017-06-12 08:59:15 -0400

    adds more PhantomJS TypeScript definitions

--- src/missing-definitions.d.ts
+++ src/missing-definitions.d.ts
@@ -5,7 +5,7 @@ declare interface PhantomJS {
     onError(msg: string, trace: any[]): void;
     exit(code: number): void;
 }
-declare type PhantomJSModule = 'webserver' | 'system' | 'webpage' | 'child_process' | 'fs';
+declare var phantom: PhantomJS;
 
 declare interface WebServerModule {
     create(): WebServer;
@@ -30,12 +30,70 @@ declare interface WebServerResponse {
     close(): void;
     closeGracefully(): void;
 }
+declare interface System {
+    args: string[];
+    env: { [name: string]: string };
+    os: {
+        architecture: string;
+        name: string;
+        version: string;
+    };
+    pid: number;
+    platform: string;
+}
+declare interface PhantomFileSystem {
+    separator: string;
+    workingDirectory: string;
+    absolute(path: string): string;
+    changeWorkingDirectory(dir: string): boolean;
+    copyTree(src: string, dest: string): void;
+    copy(src: string, dest: string): void;
+    exists(path: string): boolean;
+    isAbsolute(path: string): boolean;
+    isDirectory(path: string): boolean;
+    isExecutable(path: string): boolean;
+    isFile(path: string): boolean;
+    isLink(path: string): boolean;
+    isReadable(path: string): boolean;
+    isWritable(path: string): boolean;
+    lastModified(path: string): Date;
+    list(path: string): string[];
+    makeDirectory(path: string): boolean;
+    makeTree(path: string): boolean;
+    move(src: string, dest: string): void;
+    open(path: string, params: FileAccessParams): FileStream;
+    readLink(path: string): string;
+    read(path: string, params: FileAccessParams): string;
+    removeDirectory(path: string): void;
+    removeTree(path: string): void;
+    remove(path: string): void;
+    size(path: string): number;
+    touch(path: string): void;
+    write(source: string, content: string, params: FileAccessParams): void;
+}
 
-declare var phantom: PhantomJS;
-// declare var require: (name: PhantomJSModule) => any;
+declare type FileAccessParams = FileAccessMode | FileOptions;
+declare type FileAccessMode = 'r' | 'w' | 'a' | '+' | 'b';
+declare interface FileOptions {
+    mode: FileAccessMode;
+    charset: string;
+}
+
+declare interface FileStream {
+    atEnd(): boolean;
+    close(): void;
+    flush(): void;
+    readLine(): string;
+    read(): void;
+    seek(): void;
+    writeLine(content: string): void;
+    write(content: string): void;
+}
+
 declare var require: {
-    (name: 'fs'): any;
-    (name: 'system'): any;
+    (name: 'child_process'): any;
+    (name: 'fs'): PhantomFileSystem;
+    (name: 'system'): System;
     (name: 'webpage'): any;
     (name: 'webserver'): WebServerModule;
 };
